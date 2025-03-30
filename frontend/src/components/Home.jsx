11c28c731902ed5e9020dd50a48a6a0b67fc19d0
mincxnta <barba.cinta@gmail.com>
2025-03-31 00:12:04 +0200

    add internationalization support with i18next and language detection

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Poll } from './poll/Poll'
+import { useTranslation } from 'react-i18next'
 export const Home = () => {
+    const { t } = useTranslation();
+
     return (
         <div>
-            <h1>Home</h1>
+            <h1>{t("home")}</h1>
             <Poll />
         </div>
     )
