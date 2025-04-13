5533727954b54e6d497710a2d9ccde35b567bf34
mincxnta <barba.cinta@gmail.com>
2025-04-13 17:05:37 +0200

    refactor database configuration to support dynamic dialect selection for Sequelize

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,9 +3,24 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  port: process.env.DB_PORT,
-  logging: false
-})
+const dialect = process.env.DB_DIALECT
+
+// export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//   host: process.env.DB_HOST,
+//   dialect: process.env.DB_DIALECT,
+//   port: process.env.DB_PORT,
+//   logging: false
+// })
+
+export const sequelize = dialect === 'postgres'
+  ? new Sequelize(process.env.DB_URI, {
+    dialect,
+    logging: false
+  })
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect,
+    logging: false
+  }
+  )
