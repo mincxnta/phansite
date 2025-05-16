f6b157ddcf09c42efdee5974690133d79afe8be7
mincxnta <barba.cinta@gmail.com>
2025-05-16 18:40:36 +0200

    update ToastContainer position to bottom-left for better user experience

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <Router>
       <MusicPlayerProvider>
-        <ToastContainer position="bottom-right" 
+        <ToastContainer position="bottom-left" 
         icon={({ type }) => {
           switch (type) {
             case 'error':
