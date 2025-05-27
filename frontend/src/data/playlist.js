68670d69ff198f5a76d96c891cb41e3cfbd0db8f
mincxnta <barba.cinta@gmail.com>
2025-05-27 16:13:06 +0200

    update playlist with new music tracks on streaming and album covers

--- frontend/src/data/playlist.js
+++ frontend/src/data/playlist.js
@@ -1,18 +1,28 @@
 const playlist = [
     {
-        title: 'Beneath the Mask -instrumental version-',
-        src: '/assets/music/audio/beneath-the-mask-instrumental.mp3',
+        title: 'Beneath the Mask',
+        src: 'https://zzcolbhbkipimvedjgir.supabase.co/storage/v1/object/public/music//beneath-the-mask.mp3',
         albumCover: '/assets/music/albums/p5.jpg',
     },
     {
         title: 'Our Light',
-        src: '/assets/music/audio/our-light.mp3',
+        src: 'https://zzcolbhbkipimvedjgir.supabase.co/storage/v1/object/public/music//our-light.mp3',
         albumCover: '/assets/music/albums/p5r.jpg',
     },
     {
         title: 'You Are Stronger',
-        src: '/assets/music/audio/you-are-stronger.mp3',
+        src: 'https://zzcolbhbkipimvedjgir.supabase.co/storage/v1/object/public/music//you-are-stronger.mp3',
         albumCover: '/assets/music/albums/p5s.jpg',
+    },
+    {
+        title: 'Revolution in Your Heart',
+        src: 'https://zzcolbhbkipimvedjgir.supabase.co/storage/v1/object/public/music//revolution-in-your-heart.mp3',
+        albumCover: '/assets/music/albums/p5t.jpg',
+    },
+    {
+        title: 'Ambitions and Visions',
+        src: 'https://zzcolbhbkipimvedjgir.supabase.co/storage/v1/object/public/music//ambitions-and-visions.mp3',
+        albumCover: '/assets/music/albums/p5x.jpg',
     },
 
 ];
